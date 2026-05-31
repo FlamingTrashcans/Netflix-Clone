042152fd4a7eee1eed5b5d69aa1652e510f50a19
FlamingTrashcans <pwredman@outlook.com>
2026-05-31 01:15:08 -0500

    5/31 Update

--- netflix-clone/src/firebase.js
+++ netflix-clone/src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, getAuth } from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
